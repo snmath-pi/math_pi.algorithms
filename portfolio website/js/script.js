11aa6d88774fc5349aa40095157bc25f383487cf
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2022-01-27 21:09:11 +0530

    Delete portfolio website directory

--- portfolio website/js/script.js
+++
@@ -1,7 +0,0 @@
-/* ============================== typing animation ============================ */
-var typed = new Typed(".typing",{
-    strings:["","Learner","Competitive Programmer","C++ Developer"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
