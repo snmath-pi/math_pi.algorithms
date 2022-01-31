992046126041aef0b3f98561112d119a2c253545
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2022-01-31 23:02:38 +0530

    Update MaximumSubArray.cpp

--- MaximumSubArray.cpp
+++ MaximumSubArray.cpp
@@ -1,3 +1,6 @@
+/*To find maximum sum of k  consecutive elements in  given array*/
+
+
 #include<iostream>
 #define ll long long
 using namespace std;
@@ -11,17 +14,17 @@ int main()
     fo(i,0,n) cin>>a[i] ;
     int sum  = 0 ,maz = 0 ;
     fo(i,0,k)
-    sum+=a[i] ;
+    sum+=a[i] ;//finding sum of first k elements
     
     maz = sum ;
-    int l = 0 , r=k-1 ;
-    while(r<=n-2)
+    int l = 0 , r=k-1 ; // defining length = k 
+    while(r<=n-2)//last element we can start an array of length k is n-2
     {
         r++;
         sum+=a[r] ;
         sum-=a[l] ;
         l++ ;
-        if(sum>maz)
+        if(sum>maz)//calculating sum for all k consecutive elements
             maz=sum ;
     }
     cout<<maz;
