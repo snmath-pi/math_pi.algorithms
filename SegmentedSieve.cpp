7c552733eb3e058ad0bf04530f29a4264d52754c
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2022-02-05 23:18:53 +0530

    Update SegmentedSieve.cpp

--- SegmentedSieve.cpp
+++ SegmentedSieve.cpp
@@ -1,70 +1,61 @@
 #include<iostream>
-#include<cmath>
-#include<vector>
+#include <vector>
 #define ll long long
 #define fo(i,j,k) for(int i=j;i<k;++i)
-#define fof(i,j,k) for(int i=j;i<=k;++i)
 using namespace std;
 int sieve[1000005] ;
-void bruh(){
-    int N =1000000 ;
-    for(int i=2;i<=N;++i) {
-        sieve[i] = 1 ;
+void sieveoo(){
+    int  N =1000000 ;
+    for(int i = 2;i<=N;++i){
+        sieve[i] = 1; 
+
     }
-    for(int i=2;i*i<=N;++i){
-        if(sieve[i] == 1){
+    for(int i =2;i*i<=N;++i){
+        if(sieve[i]==1){
             for(int j=i*i;j<=N;j+=i){
                 sieve[j] = 0 ;
             }
         }
     }
 }
-void segementedsieve(){
-    int l , r ,dummy[l-r+1] ;
+void segmentedsieve(){
+    sieveoo() ;
+    int l , r  ;
     cin>>l>>r ;
-    vector<int> primes;
-    for(int i=2;i*i<=r;++i){
-        if(sieve[i]==1)
-            primes.push_back(i) ;
 
-    }
-    for(int i=0;i<=r-l+1;++i) {
+    vector<int> primes ;
+    int dummy[r-l+1] ;
+    for(int i=0;i<r-l+1;++i) {
         dummy[i] = 1 ;
     }
-    for(auto x:primes){
+    for(int i=2;i*i<=r;++i){
+        if(sieve[i]==1){
+            primes.push_back(i) ;
+        }
+    }
+    for(auto x: primes){
         int firstmultiple = (l/x) * x ;
-        if(l%x) {
+        if(l%x){
             firstmultiple+=x ;
         }
-        for(int j=max(firstmultiple,x*x);j<=r;j+=x){
-            dummy[j-l]=0 ;
-            // cout<<dummy[j-l]<<" " ;
+        for(int  j = max(firstmultiple,x*x); j<=r;j+=x){
+            dummy[j-l] =  0;
         }
-
     }
-
-    int cnt = 0 ;
-    for(int i=l;i<=r;++i){
+    int cnt = 0;
+    for(int i =l;i<=r;++i){
         if(dummy[i-l]==1) {
-            cnt++ ;
-            // cout<<dummy[i]+i<<" ";
-        } 
+            cnt++ ; 
+        }
     }
-    // for(int i=l;i<=r;++i){
-    //     if(dummy[i-l]==1)
-    //         cout<<dummy[i-l]<<" " ;
-    // }
-    // cout<<"\n" ;
-    cout<<cnt;
+    
+    cout<<cnt<<"\n" ;
 
 }
-
 int main()
 {
-    
-    bruh() ;
-    segementedsieve() ;
-
+    sieveoo() ;
+    segmentedsieve() ;
     
     
 }
