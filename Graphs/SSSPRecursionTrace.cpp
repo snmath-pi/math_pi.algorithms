4563cb34a93bf6bcb6694eb46e38faf9e8298fb9
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2023-04-04 11:05:52 +0530

    Create SSSPRecursionTrace.cpp

---
+++ Graphs/SSSPRecursionTrace.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+ 
+#define rep(i,n) for(int i=0;i<n;++i)
+#define repn(i,n) for(int i=1;i<=n;++i)
+#define rng(i, a, b) for(int i=a; i<b; i++)
+#define vi vector<int>
+#define vl vector<long long>
+#define pi pair<int, int> 
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int) (x).size()
+#define ll long long
+#define fi first
+#define se second
+#define pb push_back
+#define mpr make_pair
+ 
+using namespace std;
+ 
+int t, n, m;
+
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m >> t;
+    t--;
+    vector<vi> adj(n);
+    rep(i, m) {
+        int a, b;
+        cin >> a >> b;
+        a--, b--;
+        adj[a].pb(b);
+        adj[b].pb(a);
+    }
+
+    vi dist(n), p(n);
+    vector<bool> vis(n);
+    dist[0] = 1;
+    queue<int> q;
+    q.push(0);
+    p[0] = -1;
+    vis[0] = 1;
+    while(!q.empty()) {
+        int y = q.front(); q.pop();
+        
+        for(auto x: adj[y]) {
+            if(!vis[x]) {
+                vis[x] = 1;
+                dist[x] = dist[y] + 1;
+                p[x] = y;
+                q.push(x);
+            }
+        }
+    }
+    
+    if(!vis[t]) {
+        cout << "NOPath\n";
+    } else {
+        int s = 0;
+        if(!vis[t]) {
+            cout << "NOPath";
+            return 0;
+        }
+        function<void(int)> printpath = [&](int u) {
+            if(u == s) {
+                cout << s + 1 << ' '; return;
+            }
+            printpath(p[u]);
+            cout << u + 1 << ' ';
+        };
+        cout << "Path: "; printpath(t);
+
+    }
+
+
+
+}
